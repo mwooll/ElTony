85cd4d7e85c3fa4d28259684e9dcbbdf3d0374d0
Matija Zver <matijazver7@gmail.com>
2023-11-21 17:15:22 +0100

    cleaning the comments

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,17 +13,14 @@ from flask_restx import Resource, Api
 from backend.src.model import Pokemon
 from flask import request
 
-# Configure Flask:
-
-
 app = Flask(__name__)
-# allow access from any frontend
+
 cors = CORS()
 cors.init_app(app, resources={r"*": {"origins": "*"}})
 api = Api(app)
 
 
-pokemon_data = pd.read_csv('pokemon.csv') # Your Pokemon data
+pokemon_data = pd.read_csv('pokemon.csv')
 @app.route('/api/filter', methods=['OPTIONS'])
 def handle_options_request():
     response = jsonify({'status': 'OK'})
@@ -74,32 +71,31 @@ def recommend_endpoint():
 
 class PokemonList(Resource):
     def get(self):
-        # Load Pokémon data from a local CSV file:
+        
         with open('pokemon.csv', newline='') as f:
             reader = csv.DictReader(f)
             data = list(reader)
 
-        # Convert data to Pokemon objects:
+        
         pokemons = [Pokemon(**{k: v if v != '' else None for k, v in item.items()}) for item in data]
 
         return [pokemon.to_json() for pokemon in pokemons]
 
 class PokemonResource(Resource):
     def get(self, name):
-        # Load Pokémon data from a local CSV file:
+        
         with open('pokemon.csv', newline='') as f:
             reader = csv.DictReader(f)
             data = list(reader)
 
-        # Convert data to Pokemon objects:
+        
         pokemons = [Pokemon(**item) for item in data]
 
-        # Find the Pokémon with the given name:
+        
         for pokemon in pokemons:
             if pokemon.Name == name:
                 return pokemon.to_json()
 
-        # If no Pokémon with the given name was found, return an error:
         return {"error": "Pokémon not found"}, 404
 
 api.add_resource(PokemonList, '/pokemons')
