b4d96e4d36187a4565ce95a4655329a14a4fbc8f
Matija Zver <matijazver7@gmail.com>
2023-11-25 20:19:14 +0100

    adjusting main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import Vue from 'vue';
-import VueApexCharts from 'vue-apexcharts';
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .mount('#app')
-
-  Vue.use(VueApexCharts);
-
-Vue.component('apex-chart', VueApexCharts);
